fc821df88173f8300ca213fdaf32c4945ce3cecd
Michaelangelo Valente <miky10.mv@gmail.com>
2022-03-02 15:00:30 +0100

    singleNumber fixed tiny bug on for loop. Now it should work...

--- 136_single_number/singleNumber.c
+++ 136_single_number/singleNumber.c
@@ -35,7 +35,7 @@ Constraints:
 
 int singleNumber(int* nums, int numsSize){
     int ret = 0;
-    for( int i = 0; i < nums[i]; i++){
+    for( int i = 0; i < numsSize; i++){
         ret = nums[i] ^ ret;
     }
     return ret;
