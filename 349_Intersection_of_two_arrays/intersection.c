18cb63fcceac0b06bb8dded96ee5c753241a9cee
Michaelangelo Valente <miky10.mv@gmail.com>
2022-03-07 11:51:31 +0100

    349 done

--- 349_Intersection_of_two_arrays/intersection.c
+++ 349_Intersection_of_two_arrays/intersection.c
@@ -3,10 +3,6 @@
 
 /*HashSet exercise*/
 
-/*need to implement BST*/
-/* void insert()*/
-/* struct Node *search()*/
-/* struct Node *delete() ??? <--- DO I really need this? */ 
 
 struct node{
     int key;
@@ -67,23 +63,6 @@ struct node *searchBST( struct bst *treeRoot, int key ){
     }
     return tmp;
 }
-void printNode( struct node *root ){
-    if( root ){
-        printf("[%d] " , root->key );
-        printNode( root->left );
-        printNode( root->right );
-    }
-    return;
-}
-void printBST( struct bst *treeR ){
-    if( treeR->root == NULL ){
-        printf("No root, tree is empty.\n");
-        return;
-    }
-    printNode( *(treeR->root) );
-    return;
-}
-
 
 
 /*Hash set implmentation*/
@@ -130,20 +109,6 @@ void insertHashSet( struct hashSet *h_set, int key  ){
     return;
 }
 
-
-void printHashSet( struct hashSet *h_set ){
-    for( int i = 0 ; i < h_set->hash_maxSize; i++ ){
-        int idx = getKey( h_set, i );
-        printf("Bucket number:[_ %d _]\n", i);
-        if(h_set->Roots[idx] == NULL ){
-            printf( "Empty bucket: :(\n" );
-        }else{
-            printBST( h_set->Roots[idx] );
-        }
-        printf("\n---------------------------------------------\n\n");
-    }
-    return;
-}
 /*struct Node *searchHashSet(  )*/
 
 /*
@@ -210,14 +175,12 @@ int* intersection(int* nums1, int nums1Size, int* nums2, int nums2Size, int* ret
     allHashValues( set2, tmp, tmp_idx );
     
     /*compare set1 with values in set2(tmp) if the is present add to ret[]*/
-    int *ret_size = calloc( 1, sizeof( int ));
     int *ret = calloc( *tmp_idx, sizeof( int ) );
-
     for( int i = 0; i < *tmp_idx; i++ ){
         struct node *cmp = searchHashSet( set1, tmp[i]);
         if( cmp ){ /* set1 contains the examined value in set2 ( tmp[i] ) --> t
         here's an intersection */
-            ret[ (*ret_size)++ ] = tmp[i];
+            ret[ (*returnSize)++ ] = tmp[i];
         }
     }
 
@@ -228,76 +191,6 @@ int* intersection(int* nums1, int nums1Size, int* nums2, int nums2Size, int* ret
     *tmp_idx = 0;
     tmp_idx = 0;
     tmp = 0;
-
-
-
     
     return ret;
-}
-
-
-int main( int argc, char *argv[] ){
-    /*struct bst *nTree = newTree( 7 );
-    printBST( nTree );
-    insertBST( nTree, 8 );
-    insertBST( nTree, 3 );
-    insertBST( nTree, 10 );
-
-    insertBST( nTree, 1 );
-    insertBST( nTree, 6 );
-    insertBST( nTree, 4 );
-    insertBST( nTree, 7 );
-
-    insertBST( nTree, 14 );
-    insertBST( nTree, 13 );
-
-    printBST( nTree );
-    printf("\n-----------------------------------------------\n\n");
-    int searchfor = 1;
-    struct node *found = searchBST( nTree, searchfor );
-    if( found != NULL ){
-        printf("%d is present!\n", found->key );    
-    }else{
-        printf("%d not found.\n" , searchfor );
-    }*/
-    
-
-
-    /*struct hashSet* new_hs = newHashSet( maxSizeHash );
-    
-    insertHashSet( new_hs, 8 );
-    insertHashSet( new_hs, 15 );
-
-    insertHashSet( new_hs, 7 );
-    insertHashSet( new_hs, 14 );
-    insertHashSet( new_hs, 14 );/*This wont get added to the hash
-    printHashSet( new_hs );
-
-    printf("\n-----------------------------------------------\n\n");
-    int searchfor = 14;
-    struct node *found = searchHashSet( new_hs, searchfor );
-    if( found != NULL ){
-        printf("%d is present!\n", found->key );    
-    }else{
-        printf("%d not found.\n" , searchfor );
-    }*/
-    /*
-    Example 1:
-    Input: nums1 = [1,2,2,1], nums2 = [2,2]
-    Output: [2]
-
-    Example 2:
-    Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
-    Output: [9,4]
-    Explanation: [4,9] is also accepted. [4,9,5] [9,4,8] -->[4,9] | [9,4,8] [4,9,5] --> [9,4]
-    */
-   int nums1[] = {4, 9, 5};
-   int size1 = sizeof( nums1 )/ sizeof( nums1[0] );
-
-   int nums2[] = {9, 4, 9, 8, 4};
-   int size2 = sizeof( nums2 )/ sizeof( nums2[0] );
-
-    int *returnSize = calloc( 1, sizeof( int ));
-    int *arr = intersection( nums1, size1, nums2, size2, returnSize );
-
 }
