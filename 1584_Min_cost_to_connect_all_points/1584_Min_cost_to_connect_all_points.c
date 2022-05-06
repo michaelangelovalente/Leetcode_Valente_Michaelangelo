025e851111181ddf42107994ae554391617cf05a
Michaelangelo Valente <miky10.mv@gmail.com>
2022-05-06 08:08:50 +0200

    Replaced heap, with quicksort

--- 1584_Min_cost_to_connect_all_points/1584_Min_cost_to_connect_all_points.c
+++ 1584_Min_cost_to_connect_all_points/1584_Min_cost_to_connect_all_points.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
 /**
  * @file 1584_Min_cost_to_connect_all_points.c
  * 
@@ -78,165 +79,98 @@
 
     
 
-    Quick sort the edges by weight ( A better implementation, that might be more complexy, could be 
-    done by using a priority queue DS, We store our edge, using manhattan distance as the key value, and 
-    extract n ( number of vertices) edges, we then add the key values of the edges extracted, and obtain the solution.)
+    (3) We then QuickSort the edges by weight/key.
+    ( A better implementation, and more complex solution, can be 
+    done by using a priority queue DS, we store our edge, using manhattan distance as the key/weight value, and 
+    extract n ( number of vertices) edges, we then add the key values of the edges extracted, 
+    and obtain the solution.)
     ------ 
 */
 
-/** ----------------- Prototypes -----------------------*/
-
-
-// ------- Priority queue prototypes------ //
-struct edge;
-struct Pq *pq;
-
-// main functions
-struct Pq *makePq( int size );
-void insertPq( struct Pq *pq, int x, int y, int key );
-struct edge *extract( struct Pq *pq );
-
-//auxiliary functions
-void heapifydown( struct Pq *pq , int parent_idx );
-void heapifyup( struct Pq *pq , int i );
-struct edge *new_edge( int x, int y, int key);
-int getKey(struct edge *e);
-
-void swap(struct edge **e1, struct edge **e2 );
-int father( int child);
-int leftChild(int father );
-int rightChild(int father);
-
-
-void printPq(struct Pq *pq);
-// -------------------------------------------------- //
-
-
-
-/***** Priority queue Abstract Data type ( using the Min Heap Data Structure ) */
 struct edge{
     int vrtx_x, vrtx_y;
     int weight;
 };
-struct Pq{
-    struct edge **Edge;
-    int size;
-    int max_size;
-};
+
+/** ----------------- Prototypes -----------------------*/
+// ------- QuickSort prototypes------ //
+void QuickSort( struct edge **array, int size );
+void procedureQuickSort( struct edge **array, int i, int f );
+//auxiliary functions
+int getKey(struct edge *e);
+void swap(struct edge **e1, struct edge **e2 );
+int partition( struct edge **array, int i, int j );
+
+// -------------------------------------------------- //
+// ------- Union-Find (by rank) DS prototypes ------ //
+struct rankUnionFind;
+struct rankUnionFind *makeset(int size);
+
+int find( int a, struct rankUnionFind *data);
+bool q_unionbyRank( struct rankUnionFind *ds, int a, int b);
 
 
-/** 
- * makePq( int size ) takes an integer size, and creates the array where we will store the
- * vertices of a Priority queue.
- */
-struct Pq *makePq( int size ){
-    struct Pq *pq = calloc(1, sizeof(struct Pq ));
-    pq->Edge = calloc(size+1, sizeof( struct edge *));//this is size+1 because we will ignore position 0.
-    pq->max_size = size+1;
-    return pq;
-}
+int manhDist( int *a, int *b );
 
+void print_disjS( struct rankUnionFind *ds, int size);
 
-void insertPq( struct Pq *pq, int x, int y, int key ){
-    //New Edge 
-   /* struct edge *new_edge = calloc( 1, sizeof( struct edge *));
-    new_edge->vrtx_x = x;
-    new_edge->vrtx_y = y;
-    new_edge->weight = key;
+// -------------------- The leetcode solution Prototypes ---------------------- //
+int minCostConnectPoints(int** points, int pointsSize, int* pointsColSize);
+struct edge *new_edge( int x, int y, int key );
 
-    pq->Edge[++pq->size] = new_edge;//insert new edge
-    */
-    ++pq->size;
-    int idx = pq->size;
-    pq->Edge[idx] = calloc( 1, sizeof( struct edge *));
-    pq->Edge[idx]->vrtx_x = x;
-    pq->Edge[idx]->vrtx_y = y;
-    pq->Edge[idx]->weight = key;
-
-    
-    heapifyup( pq, pq->size );
+/***** Quick-Sort */
+void QuickSort( struct edge **array, int size ){
+    procedureQuickSort( array, 0, size );
     return;
-
 }
 
-struct edge *extract( struct Pq *pq ){
-    struct edge *retEdge = pq->Edge[1];
-    swap( &(pq->Edge[1]) , &( pq->Edge[pq->size] ) );
-    pq->size--;
-    heapifydown( pq, 1 );
-    return retEdge;
-}
-
-/**Auxiliary functions*/ //
-void heapifydown( struct Pq *pq , int parent_idx ){
-    // if rightchild or leftchild > size --> no more children
-    int left_child = leftChild( parent_idx);
-    int right_child = rightChild(parent_idx);
-    int smallest_child;
-    if( left_child <= pq->size ){ // parent has at least one child
-        if( left_child == pq->size){ // parent has only one child
-            smallest_child = left_child;
-        }else{
-            smallest_child =  getKey( pq->Edge[left_child] ) < getKey( pq->Edge[right_child] ) ? left_child : right_child;
-        }
-
-        if( getKey(pq->Edge[smallest_child]) < getKey(pq->Edge[parent_idx]) ){
-            swap( &(pq->Edge[smallest_child]) , &(pq->Edge[parent_idx]) );
-            heapifydown( pq, smallest_child );
-        }
-    }
-}
-
-void heapifyup( struct Pq *pq , int i ){ // <--- NEED TO FIX THIS
-    if( i > 1 ){
-        int parent = father(i);
-        int keyParent = getKey(pq->Edge[parent]);
-        int keyChild = getKey( pq->Edge[i] );
-        if( parent >= 1 && keyParent > keyChild ){
-            swap( &(pq->Edge[parent]), &(pq->Edge[i]) );
-            heapifyup( pq, parent);
-        }
+void procedureQuickSort( struct edge **array, int i, int f ){
+    if( f-i > 1 ){
+        int m = partition( array, i, f );
+        procedureQuickSort( array, i, m );
+        procedureQuickSort( array, m+1, f );
     }
     return;
 }
 
 
 
-struct edge *new_edge( int x, int y, int key){
-    struct edge *e = calloc( 1, sizeof( struct edge ));
-    e->vrtx_x = x;
-    e->vrtx_y = y;
-    e->weight = key;
-}
 
-void printPq(struct Pq *pq){
-    for( int i = 1; i <= pq->size; i++){
-        printf("Edge[%d] -> vertices: ( %d, %d) - [key:%d]\n",i, pq->Edge[i]->vrtx_x, pq->Edge[i]->vrtx_y, getKey(pq->Edge[i]) );
+/*** Auxiliary functions ***/
+int partition( struct edge **array, int i, int j ){
+    int pivot = getKey(array[i]);
+    int left = i, right = j;
+    while( left < right ){
+        do{
+        right--;
+        }while( getKey(array[right]) > pivot );
+
+        do{
+            left++;
+        }while( left < right && getKey(array[left]) <= pivot );
+
+        if( left < right ){
+            swap( &(array[left]), &(array[right]) );
+        }
     }
+
+    swap( &(array[i]) , &(array[right]) );
+
+    return right;
 }
 
 
+
+// ---- auxiliary function ---- //
 void swap(struct edge **e1, struct edge **e2 ){
     struct edge *tmp = *e1;
     *e1 = *e2;
     *e2 = tmp;
 }
+
 int getKey(struct edge *e){
     return e->weight;
 }
-int father( int child){
-    return child/2;
-}
-
-int leftChild(int father ){
-    return father*2;
-}
-
-int rightChild(int father){
-    return father*2+1;
-}
-
-
 
 /***** Union-Find (quickUnion by rank variation) Data Structure */
 struct rankUnionFind {
@@ -305,39 +239,39 @@ void print_disjS( struct rankUnionFind *ds, int size){
 
 /*-------------------  The leetcode Solution  -----------------*/
 /**manhattan distance <- |xi - xj| + |yi - yj|*/
+
+void printArrayE( struct edge **array, int size ){
+    for( int i = 0; i < size; i++){
+        printf("(%d, %d)  |",i ,i );
+    }
+    printf("\n");
+    for( int i = 0; i < size; i++){
+        printf("key[%d] |", getKey( array[i]));
+    }
+    printf("\n");
+}
+int minCostConnectPoints(int** points, int pointsSize, int* pointsColSize){
+    return 0;
+}
+
 int manhDist( int *a, int *b ){
     return   abs( a[0] - b[0] ) + abs( a[1] - b[1]  );
 }
 
-int minCostConnectPoints(int** points, int pointsSize, int* pointsColSize){
-    
-    struct Pq *PQ = makePq( pointsSize );
-    // Generate all the edges and store the edges in a priority queue
-        for( int i = 0; i < pointsSize; i++){
-            //dist <-- manhattan distance between vertex: i  and vertex: i+1
-            for(int j = i; j < pointsSize; j++){
-                if( i != j ){
-                    int dist = manhDist( points[i], points[j]);
-                    printf("(%d,%d)k:%d\n", i, j, dist);
-                    insertPq( PQ, i, j, dist);    
-                }
-                
-            }
-            
-
-        }
-        printf("\n--------------------------------------\n");
-        printPq(PQ);
-
-    return 0;
+struct edge *new_edge( int x, int y, int key ){
+    struct edge *new = calloc( 1, sizeof( struct edge ));
+    new->vrtx_x = x;
+    new->vrtx_y = y;
+    new->weight = key;
 }
+
 
 int main(int argc, char *argv[]){
 
-    
-    int points[][2] = {{0,0},{2,2},{3,10},{5,2},{7,0}};
+    /*
+    int points[][2] = {{0,0},{2,2},{3,10},{5,2},{7,0}};*/
     /*int points[][2] = {{3,12},{-2,5},{-4,1}};*/
-    
+    /*
     int sizeArr = sizeof( points)/ sizeof(points[0]);
     int **ptr_points = calloc( sizeArr, sizeof( int *));
     for( int i = 0; i < sizeArr; i++ ){
@@ -357,7 +291,26 @@ int main(int argc, char *argv[]){
 
     printf("--------------------------------\n");
     int colSize = 2;
-    int tmp = minCostConnectPoints( ptr_points, sizeArr, &colSize );
+    int tmp = minCostConnectPoints( ptr_points, sizeArr, &colSize );*/
 
 
+    /*
+    int arr[] = { 24, 8, 31, 16, 17, 40, 29, 23, 27};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    struct edge **arrayE = calloc( size, sizeof( struct edge *));
+    for( int i = 0; i < size; i++ ){
+        arrayE[i] = new_edge( i,i, arr[i] );
+    }
+
+    printArrayE( arrayE, size );
+    printf("\nAfter partition\n");
+
+    printf("returned pivot:%d\n",partition( arrayE, 0, size));
+    printArrayE( arrayE, size );
+
+    printf("\nAfter sort\n");
+    QuickSort( arrayE, size);
+    printArrayE( arrayE, size );
+    */
 }
